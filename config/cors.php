e3be28602631d18e9adffc6d61d98c297e392b7c
qduggit <quocdung25cm@gmail.com>
2025-04-09 11:54:08 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://quiz-frontend-sage.vercel.app'],
+    'allowed_origins' => ['https://your-react-site.netlify.app'],
     'allowed_headers' => ['*'],
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
