146cf140196b3addaccc8b4273c735e5b45f7f35
qduggit <quocdung25cm@gmail.com>
2025-04-09 11:54:40 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://your-react-site.netlify.app'],
+    'allowed_origins' => ['https://kaleidoscopic-torrone-44fa05.netlify.app'],
     'allowed_headers' => ['*'],
     'supports_credentials' => false,
 ];
